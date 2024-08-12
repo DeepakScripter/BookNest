e857d0f6afc49611c10e2ca49f4e8c2261138677
Deepak Vishwakarma <149933593+DeepakScripter@users.noreply.github.com>
2024-08-12 19:02:29 +0530

    adddowload excel add controllers

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using BookNest.Models.Domain;
 using BookNest.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using System.IO;
+using System.Linq;
 
 namespace BookNest.Controllers
 {
@@ -90,5 +94,57 @@ namespace BookNest.Controllers
             var data = bookService.GetAll();
             return View(data);
         }
+
+
+        public IActionResult DownloadExcel()
+        {
+            // Fetch all books from the service
+            var books = bookService.GetAll();
+
+            // Set the license context for EPPlus (NonCommercial or Commercial based on your license)
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                // Create a new worksheet named "Books"
+                var worksheet = package.Workbook.Worksheets.Add("Books");
+
+                // Add headers to the first row
+                worksheet.Cells[1, 1].Value = "Title";
+                worksheet.Cells[1, 2].Value = "Genre";
+                worksheet.Cells[1, 3].Value = "ISBN";
+                worksheet.Cells[1, 4].Value = "Total Pages";
+                worksheet.Cells[1, 5].Value = "Author";
+                worksheet.Cells[1, 6].Value = "Publisher";
+
+                // Populate the worksheet with book data starting from the second row
+                int row = 2;
+                foreach (var book in books)
+                {
+                    worksheet.Cells[row, 1].Value = book.Title;
+                    worksheet.Cells[row, 2].Value = book.GenreName;
+                    worksheet.Cells[row, 3].Value = book.Isbn;
+                    worksheet.Cells[row, 4].Value = book.TotalPages;
+                    worksheet.Cells[row, 5].Value = book.AuthorName;
+                    worksheet.Cells[row, 6].Value = book.PublisherName;
+                    row++;
+                }
+
+                // Save the package to a memory stream
+                var stream = new MemoryStream();
+                package.SaveAs(stream);
+                stream.Position = 0;
+
+                // Generate a file name using the current date and time
+                var fileName = $"Books_{DateTime.Now:yyyyMMddHHmmss}.xlsx";
+
+                // Return the file as a download response
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+            }
+        }
+
+
+
+
     }
 }
